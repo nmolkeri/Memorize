26637a12bd426f74f17120a9ec4e319b276f8834
Niranjan Molkeri <nmolkeri@gmail.com>
2023-05-15 12:15:31 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Niranjan on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
